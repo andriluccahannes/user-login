e5baf5a89a90a38120e66bd72e4883c268546aae
andriluccahannes <andreyluccahannes@gmail.com>
2023-03-24 16:18:10 +0100

    init

---
+++ src/main/java/grp/project/UserLoginDriver.java
@@ -0,0 +1,18 @@
+package grp.project;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UserLoginDriver {
+
+    static Logger log = Logger.getLogger(UserLoginDriver.class.getName());
+
+    public static void main(String[] args) {
+
+        log.setLevel(Level.OFF);
+
+        log.warning("You should not ignore this");
+
+    }
+
+}
